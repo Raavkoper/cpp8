ab3d699bd044e935974000a87bafb042fae53a14
Raav Koper <rkoper@Raavs-MacBook-Pro.local>
2023-02-12 13:14:19 +0100

    added ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,11 +2,17 @@
 #define EASYFIND_H
 
 #include <iostream>
-#include <algorithm>
+#include <array>
+#include <vector>
 
 template<typename T>
-void easyfind(T t, int n) {
-	std::vector<T>::iterator it;
+typename T::iterator easyfind(T container, int n) {
+	typename T::iterator it;
+	for (it = container.begin(); it != container.end(); it++) {
+		if (*it == n)
+			return (it);
+	}
+	throw std::out_of_range("Number no excisto");
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,28 @@
 #include "easyfind.hpp"
 
 int main() {
+    std::array<int, 8> array = {5, 2, 7, 4, 9, 6, 1, 9};
+    std::vector<int> vec;
+    vec.assign(5, 3);
 
+    try {
+        int i = *easyfind(array, 7);
+        std::cout << i << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try {
+        int i = *easyfind(vec, 3);
+        std::cout << i << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try {
+        int i = *easyfind(vec, 10);
+        std::cout << i << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
